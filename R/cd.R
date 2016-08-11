4bd709f2c28ecef5eb46601006f3eee714acdc60
Granville Matheson <mathesong@gmail.com>
2016-08-11 14:00:20 +0200

    Adding cd() too

--- R/cd.R
+++ R/cd.R
@@ -1,3 +1,14 @@
 cd <- function() {
+  
+  #' Change Directory
+  #'
+  #' Function to change directory to the directory stored in the clipboard
+  #'
+  #'
+  #' @examples
+  #' cd()
+  #'
+  #' @export
+  
   setwd(readClipboard())
 }
